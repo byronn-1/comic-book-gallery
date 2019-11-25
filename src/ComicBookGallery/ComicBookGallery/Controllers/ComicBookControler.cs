0ebe6c1d646a6be74b64acceceeff1da397a49e2
Byron Lee <byronn.lee@gmail.com>
2019-11-25 05:58:02 +0000

    Added Detail Action method

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookControler.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookControler.cs
@@ -6,8 +6,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBookControler: Controller
+    public class ComicBooksControler : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
