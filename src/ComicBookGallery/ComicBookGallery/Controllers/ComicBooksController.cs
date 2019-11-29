bde37b2e9719d8dfcd1d5909f44295453f334e15
Byron Lee <byronn.lee@gmail.com>
2019-11-29 06:09:13 +0000

    Updated Comic Book Detail view to display comic book information

---
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+};
